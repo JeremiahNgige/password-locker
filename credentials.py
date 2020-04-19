bd451106bdb96941089849b4d1b86690af49e0ac
Lucas Owino <lucowish35@gmail.com>
2020-04-19 12:14:30 +0300

    added logic to credentials.py to create new instance of the credentials class

---
+++ credentials.py
@@ -0,0 +1,9 @@
+class Credentials:
+    """
+    create class for user credentials
+    """
+
+    def __init__(self, account_name, account_username, account_password):
+        self.account_name = account_name
+        self.account_username = account_username
+        self.account_password = account_password
