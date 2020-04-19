7fc16bc5d9053e6b76ad9aaeb70ab6cdc9be4d3f
Lucas Owino <lucowish35@gmail.com>
2020-04-19 15:00:27 +0300

    added a function to save credentials in run.py

---
+++ run.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3.6
+
+import random
+from user import User
+from credentials import Credentials
+
+# functions that add credentials
+
+
+def create_new_credentials(account_name, account_username, account_password):
+    """
+    function to create new acc with its credentials
+    """
+    new_credentials = Credentials(
+        account_name, account_username, account_password)
+    return new_credentials
+
+
+def save_new_credentials(credentials):
+    """
+    function to save the new credentials
+    """
+    credentials.save_credentials()
